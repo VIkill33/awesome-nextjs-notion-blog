c33a2d7b411f4244a8528a68b4ba49164c70a067
Vikill <vikill1024@163.com>
2023-04-19 02:57:59 +0800

    New:
    - add Baidu verification tag

--- site.config.js
+++ site.config.js
@@ -74,5 +74,6 @@ module.exports = {
   footCounterList: [
     '6a76fdd9ec6d4ae2855aa790d620eec4',
     '651765de-6a0f-4a72-9f91-86f26381aef3'
-  ]
+  ],
+  baidu_site_verification: codeva-14VYy4BOzh
 }
